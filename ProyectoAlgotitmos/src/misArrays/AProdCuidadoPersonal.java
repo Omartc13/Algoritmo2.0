a546c4f6b460eb4c5c7ca1a4f6de8d9cc06c17dc
Omartc13 <118316814+Omartc13@users.noreply.github.com>
2023-09-30 14:37:26 -0500

    Terminado ArrayCuidadoPersonal

--- ProyectoAlgotitmos/src/misArrays/AProdCuidadoPersonal.java
+++ ProyectoAlgotitmos/src/misArrays/AProdCuidadoPersonal.java
@@ -1,13 +1,155 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package misArrays;
+
+import java.util.ArrayList;
+import java.util.List;
+import modelo.ProdCuidadoPersonal;
 
 /**
  *
  * @author Omar Tc
  */
 public class AProdCuidadoPersonal {
+    
+    private ArrayList <ProdCuidadoPersonal> makeup;
+
+    public AProdCuidadoPersonal() {
+        makeup=new ArrayList<ProdCuidadoPersonal>();
+    }
+
+    //agregar makeup
+    public void agregarProductoM(ProdCuidadoPersonal mak){
+        makeup.add(mak);
+    }
+    
+    //eliminar makeup
+    public void eliminarProductoM(int nroMake){
+        makeup.remove(nroMake);
+    }
+    
+    public ProdCuidadoPersonal obtener (int posicion){
+        return makeup.get(posicion);
+    }
+    
+    public int tamañoArreglo(){
+        return makeup.size();
+    }
+    
+    public void mergeSortCod(){
+        mergeSortCod(0, makeup.size()-1);
+    }
+    
+    private void mergeSortCod(int izq, int der){
+        if (izq<der) {
+            int medio=(izq+der)/2;
+            //Ordenar mitades de Izq y Der
+            mergeSortCod(izq, medio);
+            mergeSortCod(medio+1, der);
+            //Conbinar mitades ordenadas
+            mergeCod(izq, medio, der);
+            
+        }
+    }
+    
+    private void mergeCod(int izquierda, int medio, int derecho){
+        int n1= medio-izquierda+1;
+        int n2= derecho-medio;
+        
+        //Listas Temporales 
+        List<ProdCuidadoPersonal> listaIzquierda= new ArrayList<>();
+        List<ProdCuidadoPersonal> listaDerecha= new ArrayList<>();
+        
+        //Copiar los datos a los Arrays nuevos
+        
+        for (int i = 0; i < n1; i++) {
+            listaIzquierda.add(obtener(izquierda+i));
+        }
+        for (int j = 0; j < n2; j++) {
+            listaDerecha.add(obtener(medio+1+j));
+        }
+        
+        //Combinar los Arrays
+        int i=0, j=0, k=izquierda;
+        while (i<listaIzquierda.size() && j<listaDerecha.size()) {
+            if (listaIzquierda.get(i).getCodigo() <= listaDerecha.get(j).getCodigo()) {
+                makeup.set(k, listaIzquierda.get(i));
+                i++;
+            }else{
+                makeup.set(k, listaDerecha.get(j));
+                j++;
+            }
+            k++;
+        }
+        //Copiar los elementos restantes de der o izq si hay
+        
+        while (i<n1) {            
+            makeup.set(k, listaIzquierda.get(i));
+            i++;
+            k++;
+        }
+        while (j<n2) {            
+            makeup.set(k, listaDerecha.get(j));
+            j++;
+            k++;
+        }
+    }
+    
+    public void mergeSortNom(){
+        mergeSortNom(0, tamañoArreglo()-1);
+    }
+    
+    private void mergeSortNom(int izq, int der){
+        if (izq<der) {
+            int medio=(izq+der)/2;
+            //Ordenar mitades de Izq y Der
+            mergeSortNom(izq, medio);
+            mergeSortNom(medio+1, der);
+            //Conbinar mitades ordenadas
+            mergeNom(izq, medio, der);
+            
+        }
+    }
+    
+    private void mergeNom(int izquierda, int medio, int derecho){
+        int n1= medio-izquierda+1;
+        int n2= derecho-medio;
+        
+        //Listas Temporales 
+        List<ProdCuidadoPersonal> listaIzquierda= new ArrayList<>();
+        List<ProdCuidadoPersonal> listaDerecha= new ArrayList<>();
+        
+        //Copiar los datos a los Arrays nuevos
+        
+        for (int i = 0; i < n1; i++) {
+            listaIzquierda.add(obtener(izquierda+i));
+        }
+        for (int j = 0; j < n2; j++) {
+            listaDerecha.add(obtener(medio+1+j));
+        }
+        
+        //Combinar los Arrays
+        int i=0, j=0, k=izquierda;
+        while (i<listaIzquierda.size() && j<listaDerecha.size()) {
+            if (listaIzquierda.get(i).getNombre_producto().compareToIgnoreCase(listaDerecha.get(j).getNombre_producto()) <= 0) {
+                makeup.set(k, listaIzquierda.get(i));
+                i++;
+            }else{
+                makeup.set(k, listaDerecha.get(j));
+                j++;
+            }
+            k++;
+        }
+        //Copiar los elementos restantes de der o izq si hay
+        
+        while (i<n1) {            
+            makeup.set(k, listaIzquierda.get(i));
+            i++;
+            k++;
+        }
+        while (j<n2) {            
+            makeup.set(k, listaDerecha.get(j));
+            j++;
+            k++;
+        }
+    }
     
 }
